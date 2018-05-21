32548c4676737b058e7240925c045e20e10ccf4e
Michael Muller <mullermike0521@yahoo.com>
2018-05-21 09:46:18 -0500

    Add project files.

---
+++ 0503 Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0503_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your favorite number?");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick another random number.");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The numbers you picked are " + a + " and " + b);
+            int c = a + b;
+            Console.WriteLine("The sum of these numbers is " + c);
+            Console.ReadKey();
+        }
+    }
+}
